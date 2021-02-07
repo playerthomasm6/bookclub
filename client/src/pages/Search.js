bfb2de08682233b698befec5ff35411aa1f845a5
playerthomasm6 <69253499+playerthomasm6@users.noreply.github.com>
2021-02-07 18:39:25 -0500

    added google search books to page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,32 +1,39 @@
 import React, { useState, useEffect } from "react";
 import API from "../utils/API";
+import googleAPI from "../utils/googleAPI";
 import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
 import { Link } from "react-router-dom";
 import DeleteBtn from "../components/DeleteBtn";
+import GoogleSearch from "../components/GoogleSearch";
+import SearchContext from "../utils/searchContext";
+import "./style.css";
 
 function Search(props) {
-    const [books, setBooks] = useState([]);
-    const [formObject, setFormObject] = useState({});
+  const [books, setBooks] = useState([]);
+  const [formObject, setFormObject] = useState({});
+  const [googleBooks, setGoogleBooks] = useState([]);
+  const [search, setSearch] = useState();
 
-     useEffect(() => {
-       
-         loadBooks()
-         console.log(books);
-     }, [] 
-     )
-    
-    // Loads all books and sets them to books
+  useEffect(() => {
+
+    loadBooks()
+    // loadGoogleResults("Harry+Potter")
+    console.log(books);
+  }, []
+  )
+
+  // Loads all books and sets them to books
   function loadBooks() {
     API.getBooks()
       .then(res =>
-         setBooks(res.data)
+        setBooks(res.data)
       )
       .catch(err => console.log(err));
   };
 
-   // Deletes a book from the database with a given id, then reloads books from the db
-   function deleteBook(id) {
+  // Deletes a book from the database with a given id, then reloads books from the db
+  function deleteBook(id) {
     API.deleteBook(id)
       .then(res => loadBooks())
       .catch(err => console.log(err));
@@ -34,79 +41,146 @@ function Search(props) {
 
   function handleInputChange(event) {
     const { name, value } = event.target;
-    setFormObject({...formObject, [name]: value})
+    setFormObject({ ...formObject, [name]: value })
   };
 
-  function handleFormSubmit(event) {
+  //  function handleFormSubmit(event) {
+  //    event.preventDefault();
+  //   if (formObject.title && formObject.author) {
+  //      API.saveBook({
+  //        title: formObject.title,
+  //        author: formObject.author,
+  //        synopsis: formObject.synopsis
+  //      })
+  //        .then(res => loadBooks())
+  //        .catch(err => console.log(err));
+  //    }
+  //  };
+
+  function handleFormSubmit(event, search) {
     event.preventDefault();
-    if (formObject.title && formObject.author) {
-      API.saveBook({
-        title: formObject.title,
-        author: formObject.author,
-        synopsis: formObject.synopsis
-      })
-        .then(res => loadBooks())
-        .catch(err => console.log(err));
+    if (formObject.search) {
+      loadGoogleResults(formObject.search)
+
     }
   };
 
-  
-    return (
-        <div>   
-            <h6>Search for a book</h6>
-            <form>
-              <Input
-                onChange={handleInputChange}
-                name="title"
-                placeholder="Title (required)"
-              />
-              <Input
-                onChange={handleInputChange}
-                name="author"
-                placeholder="Author (required)"
-              />
-              <TextArea
-                onChange={handleInputChange}
-                name="description"
-                placeholder="Description (required)"
-              />
-              <FormBtn
-                disabled={!(formObject.author && formObject.title)}
-                onClick={handleFormSubmit}
-              >
-                Submit Book
+  function loadGoogleResults(search) {
+    googleAPI.getBooks(search)
+      .then(res => setGoogleBooks(res.data.items))
+      .then(console.log())
+      .catch(err => console.log(err))
+  }
+
+
+  return (
+    <div>
+      <form>
+        <Input
+          onChange={handleInputChange}
+          name="search"
+          placeholder="search a book"
+          value={formObject.search}
+        />
+        <FormBtn
+          disabled={!(formObject.search)}
+          onClick={handleFormSubmit}
+        >
+          Search Book
               </FormBtn>
-            </form>
-         <div>
-           
-         {books.length ? (
-              <List>
-                {books.map(book => {
-                  return (
-                    <ListItem key={book._id}>
-                      <a href={"/books/" + book._id}>
-                        <strong>
-                          {book.title} by {book.author}
-                        </strong>
-                        <img className="book-image" src={book.image}></img>
-                        
-                        <a href={book.link}>Amazon.com</a>
-                        
-                      </a>
-                      <DeleteBtn onClick={() => deleteBook(book._id)} />
-                    </ListItem>
-                  );
-                })}
-              </List>
-            ) : (
-              <h3>No Results to Display</h3>
-            )}
-         </div>
-            
-        </div>
-        
-    )
-    
+      </form>
+
+      
+
+      <div>
+        {books.length ? (
+          <List>
+            {books.map(book => {
+              return (
+                
+
+                <ListItem key={book._id}>
+                  <div className="container-fluid border">
+            <div className="row">
+
+              <div className="col-sm-3">
+                <img className="thumbNail"src={book.image} alt="bookthumbnail"></img>
+              </div>
+
+              <div className="col-sm-9">
+                <div className="row">
+              
+                  <div className="col-sm-12">
+                      <h5>{book.title}</h5>
+                      <p>{book.author}</p>
+                  </div>
+
+                <div className="row">
+                  <div className="col-sm-12">
+                      <p>{book.description}</p>
+                      <a href={book.link}>Amazon.com</a>
+                  </div>  
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+                  <DeleteBtn onClick={() => deleteBook(book._id)} />
+                </ListItem>
+              );
+            })}
+          </List>
+        ) : (
+            <h3>No Results to Display</h3>
+          )}
+      </div>
+      <div>
+        {googleBooks.map(googleBook => {
+          const buyLink = () => {
+            if(googleBook.volumeInfo.saleInfo.buyLink) {
+            return <a href={googleBook.volumeInfo.saleInfo.buyLink}>Buy this book for ${googleBook.volumeInfo.saleInfo.listPrice.amount}</a>
+          } else {
+            return <p>Book Not For Sale</p>
+          }
+        }
+        return (
+          <div className="container-fluid border">
+            <div className="row">
+
+              <div className="col-sm-2">
+                <img className="thumbNail"src={googleBook.volumeInfo.imageLinks.smallThumbnail} alt="bookthumbnail"></img>
+              </div>
+
+              <div className="col-sm-10">
+                <div className="row">
+              
+                  <div className="col">
+                      <h5>{googleBook.volumeInfo.title}</h5>
+                      <p>{googleBook.volumeInfo.authors}</p>
+                  </div>
+
+                <div className="row">
+                  <div className="col">
+                      <p>{googleBook.volumeInfo.description}</p>
+                      <a href={googleBook.volumeInfo.previewLink}>Preview This Book</a>
+                      <p>{googleBook.volumeInfo.saleablility}</p>
+                      <div>{buyLink}</div>
+                      
+                      
+                      
+                  </div>  
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+        )
+      })}
+      </div>
+    </div>
+
+  )
+
 }
 
 export default Search;
---
+++ client/src/utils/googleAPI.js
@@ -0,0 +1,24 @@
+
+
+import axios from "axios";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  // Gets all books
+  getBooks: function(search) {
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}+intitle&key=AIzaSyCfNSlok9z5LCWbnsEXzAJAvREXm4couAU`)
+  },
+  // Gets the book with the given id
+  getBook: function(id) {
+    return axios.get("/api/books/" + id);
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
+  }
+  
+};
